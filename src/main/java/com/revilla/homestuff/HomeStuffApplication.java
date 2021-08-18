29cf11c77a599dfe8a362b50563eec071fa452e5
Kirenai <pool_dota2@hotmail.com>
2021-08-18 18:11:22 -0500

    init repo

---
+++ src/main/java/com/revilla/homestuff/HomeStuffApplication.java
@@ -0,0 +1,13 @@
+package com.revilla.homestuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revilla/homestuff/HomeStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revilla.homestuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
