ce1c108ef424bf41ab7e6bfab6869e565d0f0e95
Kirenai <pool_dota2@hotmail.com>
2021-08-24 01:25:09 -0500

    Adding the missing http methods

--- src/main/java/com/revilla/homestuff/api/UserResource.java
+++ src/main/java/com/revilla/homestuff/api/UserResource.java
@@ -1,17 +1,25 @@
 package com.revilla.homestuff.api;
 
+import java.util.List;
 import com.revilla.homestuff.dto.UserDto;
 import com.revilla.homestuff.service.GeneralService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
 public class UserResource {
 
     @Qualifier("user.service")
@@ -22,9 +30,29 @@ public class UserResource {
         return this.userService.findAll(pageable);
     }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
+        UserDto response = this.userService.findOne(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @PostMapping
-    public UserDto saveUser(@RequestBody UserDto data) {
-        return this.userService.create(data);
+    public ResponseEntity<UserDto> saveUser(@RequestBody UserDto data) {
+        UserDto response = this.userService.create(data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId,
+            @RequestBody UserDto userDto) {
+        UserDto response = this.userService.update(userId, userDto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<UserDto> deleteUser(@PathVariable Long userId) {
+        UserDto response = this.userService.delete(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
