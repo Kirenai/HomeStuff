fc28478fd1f1733b2c34021669db887d92ae4d1e
Kirenai <pool_dota2@hotmail.com>
2021-08-25 16:18:34 -0500

    adding the service, repository and dto of the Role entity

---
+++ src/main/java/com/revilla/homestuff/dto/RoleDto.java
@@ -0,0 +1,22 @@
+package com.revilla.homestuff.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * RoleDto
+ * @author Kirenai
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDto {
+
+    private Long id;
+
+    private String name;
+
+}
---
+++ src/main/java/com/revilla/homestuff/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.revilla.homestuff.repository;
+
+import com.revilla.homestuff.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * RoleCategory
+ * @author Kirenai
+ */
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
