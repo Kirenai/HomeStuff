88763e1efa946bae65f0551a4ced15e2e55e12cd
Kirenai <pool_dota2@hotmail.com>
2021-09-01 02:41:33 -0500

    Improving the messages, at the time the error message will go out to the entity that belongs to it

--- src/main/java/com/revilla/homestuff/service/imp/GeneralServiceImp.java
+++ src/main/java/com/revilla/homestuff/service/imp/GeneralServiceImp.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+
 import com.revilla.homestuff.service.GeneralService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * GeneralServiceImp
+ *
  * @author Kirenai
  */
 @Slf4j
@@ -31,7 +33,7 @@ public abstract class GeneralServiceImp<T, ID extends Serializable, E> implement
     }
 
     @SuppressWarnings("unchecked") // idk, but work
-    public Class<T> getFirstGenericClass(){
+    public Class<T> getFirstGenericClass() {
         if (firstGeneric == null) {
             firstGeneric = (Class<T>) Objects.requireNonNull(GenericTypeResolver
                     .resolveTypeArguments(this.getClass(), GeneralServiceImp.class))[0];
@@ -40,41 +42,41 @@ public abstract class GeneralServiceImp<T, ID extends Serializable, E> implement
     }
 
     @SuppressWarnings("unchecked") // idk, but work
-    public Class<E> getThirdGenericClass(){
+    public Class<E> getThirdGenericClass() {
         if (thirdGeneric == null) {
             thirdGeneric = (Class<E>) Objects.requireNonNull(GenericTypeResolver
-                    .resolveTypeArguments(getClass(), GeneralServiceImp.class))[2];
+                    .resolveTypeArguments(this.getClass(), GeneralServiceImp.class))[2];
         }
         return thirdGeneric;
     }
 
     @Override
     public List<T> findAll(Pageable pageable) {
-        log.info("Calling the findAll method in " + getClass());
+        log.info("Calling the findAll method in " + this.getClass());
         return this.getRepo().findAll(pageable)
-            .getContent()
-            .stream()
-            .map(obj -> this.modelMapper.map(obj, this.getFirstGenericClass()))
-            .collect(Collectors.toList());
+                .getContent()
+                .stream()
+                .map(obj -> this.modelMapper.map(obj, this.getFirstGenericClass()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public T findOne(ID id) {
-        log.info("Calling the findOne method in " + getClass());
+        log.info("Calling the findOne method in " + this.getClass());
         E obj = this.getRepo().findById(id)
-            .orElseThrow(() -> new IllegalStateException("Don't found"));
+                .orElseThrow(() -> new IllegalStateException(this.getThirdGenericClass().getSimpleName() + " don't found with id: " + id));
         return this.modelMapper.map(obj, this.getFirstGenericClass());
     }
 
     @Override
     public T delete(ID id) {
-        log.info("Calling the delete method in " + getClass());
+        log.info("Calling the delete method in " + this.getClass());
         return this.getRepo().findById(id)
-            .map(obj -> {
-                this.getRepo().delete(obj);
-                return this.modelMapper.map(obj, this.getFirstGenericClass());
-            })
-            .orElseThrow(() -> new IllegalStateException("Don't found"));
+                .map(obj -> {
+                    this.getRepo().delete(obj);
+                    return this.modelMapper.map(obj, this.getFirstGenericClass());
+                })
+                .orElseThrow(() -> new IllegalStateException(this.getThirdGenericClass().getSimpleName() + " don't found with id: " + id));
     }
 
     public abstract JpaRepository<E, ID> getRepo();
