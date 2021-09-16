31860fd4a2f372332831d3ccee92de08ca4ba816
Kirenai <pool_dota2@hotmail.com>
2021-09-16 00:41:48 -0500

    cors configuration

---
+++ src/main/java/com/revilla/homestuff/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.revilla.homestuff.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebConfig
+ *
+ * @author Kirenai
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowCredentials(true)
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
+            .exposedHeaders("Access-Control-Allow-Headers",
+                "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, "
+                    + "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+            .maxAge(3600);
+    }
+
+}
