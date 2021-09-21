5b7e85d2afe5bc16bcfbd078046ed52116a24b65
Kirenai <pool_dota2@hotmail.com>
2021-09-21 11:03:30 -0500

    Improving indentation

--- src/main/java/com/revilla/homestuff/service/imp/UserServiceImp.java
+++ src/main/java/com/revilla/homestuff/service/imp/UserServiceImp.java
@@ -80,14 +80,16 @@ public class UserServiceImp extends GeneralServiceImp<UserDto, Long, User> imple
         return this.userRepository.findById(id)
                 .map(user -> {
                     if (user.getUserId().equals(userDetails.getUserId())
-                            || userDetails.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.name()))) {
+                            || userDetails.getAuthorities().contains(
+                                    new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.name()))) {
                         user.setUsername(data.getUsername());
                         user.setPassword(this.passwordEncoder.encode(data.getPassword()));
                         user.setFirstName(data.getFirstName());
                         user.setLastName(data.getLastName());
                         user.setAge(data.getAge());
                         if (Objects.nonNull(data.getRoles())
-                                && userDetails.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.name()))) {
+                                && userDetails.getAuthorities().contains(
+                                        new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.name()))) {
                             user.setRoles(RoleUtil.getSetOfRolesOrThrow(data.getRoles(), this.roleRepository));
                         }
                         return GeneralUtil.responseMessageAction(user, User.class, "updated successfully");
